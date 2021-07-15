6d9b4cb921f015bd0f7723a46851c673f6584a10
sharath.n <sharath.n@MMMDVSHARATHN.local>
2021-07-15 16:51:44 +0530

    commit

--- public/js/todo.js
+++ public/js/todo.js
@@ -1,1 +1,16 @@
 // put js for todo app here
+//alert("hi");
+
+var list = document.querySelectorAll(".item");
+var btn = document.getElementById("btn");
+
+console.log("list ",list);
+list.forEach(item => {
+    item.addEventListener('click', function(ev) {
+        ev.target.classList.toggle("cross");
+    });
+})
+
+btn.addEventListener('click', function(ev){
+    alert("ji");
+});
